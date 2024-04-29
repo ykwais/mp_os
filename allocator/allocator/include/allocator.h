7808c6016b8efc3c77c0286e47aa2746c536665f
ykwais <fedorkovaleks04@gmail.com>
2024-04-30 00:05:09 +0300

    breakpoint

--- allocator/allocator/include/allocator.h
+++ allocator/allocator/include/allocator.h
@@ -1,3 +1,4 @@
+
 #ifndef MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_H
 #define MATH_PRACTICE_AND_OPERATING_SYSTEMS_ALLOCATOR_H
 
@@ -9,57 +10,54 @@ class allocator
 {
 
 public:
-    
+
     typedef size_t block_size_t;
-    
+
     typedef void *block_pointer_t;
 
 public:
-    
+
     virtual ~allocator() noexcept = default;
 
 public:
-    
+
     template<
-        typename T,
-        typename ...args>
+            typename T,
+            typename ...args>
     inline static void construct(
-        T *at,
-        args... constructor_arguments);
-        args&&... constructor_arguments);
-    
+            T *at,
+            args&&... constructor_arguments);
+
     template<
-        typename T>
+            typename T>
     inline static void destruct(
-        T *at);
+            T *at);
 
 public:
-    
+
     [[nodiscard]] virtual void *allocate(
-        size_t value_size,
-        size_t values_count) = 0;
-    
+            size_t value_size,
+            size_t values_count) = 0;
+
     virtual void deallocate(
-        void *at) = 0;
-    
+            void *at) = 0;
+
 };
 
 template<
-    typename T,
-    typename ...args>
+        typename T,
+        typename ...args>
 inline void allocator::construct(
-    T *at,
-    args... constructor_arguments)
-    args&&... constructor_arguments)
+        T *at,
+        args&&... constructor_arguments)
 {
-    new(at) T(constructor_arguments...);
     new(at) T(std::forward<args>(constructor_arguments)...);
 }
 
 template<
-    typename T>
+        typename T>
 inline void allocator::destruct(
-    T *at)
+        T *at)
 {
     at->~T();
 }
